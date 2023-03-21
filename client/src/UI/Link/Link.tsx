5ac354f29cac493632a6b0a4120cb06d01ccee5b
Maksim <malcewdeveloper@yandex.ru>
2023-03-21 21:22:10 +0300

    Create a component Link

---
+++ client/src/UI/Link/Link.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import clsx from 'clsx';
+import classes from './Link.module.scss';
+
+
+interface ILinkProps {
+    addonBefore?: React.ReactNode;
+    addonAfter?: React.ReactNode;
+    children?: React.ReactNode;
+    color?: 'primary' | 'inherit' | string;
+    component?: 'button' | string;
+    href?: string;
+    underline?: 'always' | 'hover' | 'none';
+}
+
+
+const Link = (props: ILinkProps) => {
+    const { 
+        addonAfter,
+        addonBefore,
+        children,
+        color='inherit',
+        component='a',
+        href, 
+        underline='always'
+    } = props;
+
+    const styles = clsx({
+        [classes.root]: true,
+        [classes.button]: !href || component === 'button',
+        [classes.underlineNone]: underline === 'none',
+        [classes.underlineHover]: underline === 'hover',
+        [classes.underlineAlways]: underline === 'always',
+        [classes.primary]: color === 'primary',
+        [classes.inherit]: color === 'inherit'
+    })
+    
+    if(!href || component && component === 'button') {
+        return <button className={ styles }>{ children }</button>;
+    }
+
+    return <a href={ href } className={ styles }>{ children }</a>;
+
+}
+
+export default Link;
