aa3a76df235b16bcb2c2b299d315db416ba6485d
Maksim <malcewdeveloper@yandex.ru>
2023-03-24 22:23:45 +0300

    Create and add normalize global styles

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Home from "./pages/Home/Home";
+import './styles/global.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<Home />)
