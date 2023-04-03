45f3fa855302ba461a0c9ec5926a6134c9df81f6
Maksim <malcewdeveloper@yandex.ru>
2023-04-04 02:17:28 +0300

    Add possibility work with image extensions /
    * .png
    * .svg
    * .jpg

--- client/src/custom.d.ts
+++ client/src/custom.d.ts
@@ -22,3 +22,20 @@ declare module '*.module.styl' {
     const classes: { [key: string]: string };
     export default classes;
 }
+
+declare module "*.png";
+ 
+declare module "*.png" {
+    const value: any;
+    export default value;
+}
+
+declare module "*.svg" {
+    const value: any;
+    export default value;
+}
+
+declare module "*.jpg" {
+    const value: any;
+    export default value;
+}
