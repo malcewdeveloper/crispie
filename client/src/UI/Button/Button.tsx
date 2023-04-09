05417cabc88618e2c670e71a625d1381a35b159b
Maksim <malcewdeveloper@yandex.ru>
2023-04-09 12:40:39 +0300

    Update UI component (Button) /
    Added:
    * default <variant=text> value
    * three value in props variant - text | outlined | contained
    * if href than return element <a></a>
    * iconBefore add to the start of the component, iconAfter add to the end

--- client/src/UI/Button/Button.tsx
+++ client/src/UI/Button/Button.tsx
@@ -2,29 +2,51 @@ import React from "react";
 import clsx from 'clsx';
 import classes from './Button.module.scss';
 
-type Color = 'primary' | string;
-type Variant = 'contained' | 'outlined' | 'text';
-
-interface IBaseButton {
+export interface IBaseButton {
   children?: React.ReactNode;
-  color?: Color;
+  color?: 'primary' | string;
   disabled?: boolean;
-  variant?: Variant;
+  iconBefore?: React.ReactNode;
+  iconAfter?: React.ReactNode;
+  style?: React.CSSProperties;
+  variant?: 'contained' | 'outlined' | 'text';
   href?: string;
 }
 
 const Button = (props: IBaseButton): ReturnType<React.FC> => {
 
-  const { color, children, disabled, variant, href } = props;
+  const { 
+    color, 
+    children, 
+    disabled, 
+    variant='text', 
+    iconBefore,
+    iconAfter,
+    style,
+    href
+  } = props;
 
   const styles = clsx({
     [classes.root]: true,
     [classes.primary]: color !== 'primary' ? false : true,
-    [classes.disabled]: disabled
+    [classes.disabled]: disabled,
+    [classes.text]: variant === 'text',
+    [classes.outlined]: variant === 'outlined',
+    [classes.contained]: variant === 'contained'
   })
   
   return (
-    <button className={ styles }>{ children }</button>
+    href ?
+    <a className={ styles } href={ href } style={ style }>
+      { iconBefore && iconBefore }
+      { children }
+      { iconAfter && iconAfter }
+    </a> :
+    <button className={ styles } style={ style }>
+      { iconBefore && iconBefore }
+      { children }
+      { iconAfter && iconAfter }
+    </button>
   )
 }
 
