1c38cc0ce997e4a4faca9f72616732407b845497
Maksim <malcewdeveloper@yandex.ru>
2023-04-17 10:11:32 +0300

    Refactor general file index.js (server) /
    Connected:
    - cors
    - cookie-parser
    - dotenv
    
    Refactor:
    Add function start() and wrap it in the try catch block

--- server/index.js
+++ server/index.js
@@ -1,13 +1,24 @@
+require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const userRouter = require('./routes/user-routes')
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello postgress + nodejs');
-});
+app.use(express.json());
+app.use('/api', userRouter);
 
-app.listen(PORT, () => {
-    console.log(`Server started on port ${PORT}`);
-});
+
+const start = () => {
+    try {
+        app.listen(PORT, () => {
+            console.log(`Server started on port ${PORT}`);
+        });
+    }catch(error) {
+        console.log(error);
+    }
+}
+start();
