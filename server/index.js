786a70c53848e50310e136a737fbc91685479eb2
Maksim <malcewdeveloper@yandex.ru>
2023-04-18 11:16:53 +0300

    Refactor general file index.js (server) /
    Refactor:
    Create a root router and add in the project

--- server/index.js
+++ server/index.js
@@ -2,14 +2,16 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
-const userRouter = require('./routes/user-routes')
+const rootRouter = require('./routers/root-router');
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 
 app.use(express.json());
-app.use('/api', userRouter);
+app.use(cookieParser());
+app.use(cors());
+app.use('/api', rootRouter);
 
 
 const start = () => {
