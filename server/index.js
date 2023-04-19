f4e41238e600b3627ef13e4f0f1f70d65abf0a3c
Maksim <malcewdeveloper@yandex.ru>
2023-04-19 22:15:16 +0300

    [index.js] Add a middleware to handle errors (server)

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const rootRouter = require('./routers/root-router');
+const errorMiddleware = require('./middlewares/error-middleware');
 
 const PORT = process.env.PORT || 5000;
 
@@ -12,6 +13,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
 app.use('/api', rootRouter);
+app.use(errorMiddleware);
 
 
 const start = () => {
