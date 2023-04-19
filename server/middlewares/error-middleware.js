5473060790174de37ea5a34a218e5d427c91a4ce
Maksim <malcewdeveloper@yandex.ru>
2023-04-19 22:52:11 +0300

    Create a middleware to work with errors

---
+++ server/middlewares/error-middleware.js
@@ -0,0 +1,8 @@
+const ApiError = require('../exceptions/api-error');
+
+module.exports = function(error, req, res, next) {
+    if(error instanceof ApiError) {
+        return res.status(error.status).json({message: error.message, errors: error.errors});
+    }
+    return res.status(500).json({message: 'Непредвиденная ошибка'});
+}
