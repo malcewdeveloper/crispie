10cdbb459d5b8ec1fba0ffa0e7bde02daad4afea
Maksim <malcewdeveloper@yandex.ru>
2023-04-22 21:41:43 +0300

    [index.tsx] Refactor the entry point to the app (client)

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Home from "./pages/Home/Home";
+import App from "./App";
 import './styles/global.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Home />)
+root.render(
+    <App />
+)
