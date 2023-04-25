91ad5df340af1277fed20e34d8fbfb91e0422a57
Maksim <malcewdeveloper@yandex.ru>
2023-04-25 23:06:55 +0300

    Create a UI component Typography (client)

---
+++ client/src/UI/Typography/Typography.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import clsx from "clsx";
+import classes from './Typography.module.scss';
+import useCreateElement from "../../utils/hooks/useCreateElement";
+
+
+export interface ITypographyProps {
+    children?: React.ReactNode;
+    className?: string;
+    component?: React.ElementType;
+    variant?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'paragraph' | 'body' | string;
+    marginBottom?: boolean;
+}
+
+export default function Typography(props: ITypographyProps): ReturnType<React.FC> {
+
+    const { 
+        children, 
+        className, 
+        component,
+        variant = 'body',
+        marginBottom = false
+    } = props;
+
+    const styles = clsx({
+        [classes.root]: true,
+        [classes.h1]: variant === 'h1',
+        [classes.h2]: variant === 'h2',
+        [classes.h3]: variant === 'h3',
+        [classes.h4]: variant === 'h4',
+        [classes.h5]: variant === 'h5',
+        [classes.h6]: variant === 'h6',
+        [classes.paragraph]: variant === 'paragraph',
+        [classes.body]: variant === 'body',
+        [classes.marginBottom]: marginBottom,
+        [className]: className
+    })
+
+    const variantMaping = {
+        h1: 'h1',
+        h2: 'h2',
+        h3: 'h3',
+        h4: 'h4',
+        h5: 'h5',
+        h6: 'h6',
+        paragraph: 'p',
+        body: 'span'
+    }
+    const tagName = component || variantMaping[variant] || 'span';
+
+    const Component = useCreateElement(tagName, { className: styles }, children);
+
+    return Component as React.ReactElement;
+}
