1ca5b5a88557c325ec864f51b4c6d50cb17ba1f0
Maksim <malcewdeveloper@yandex.ru>
2023-05-06 23:57:47 +0300

    [SliderControll] Refactor the component SliderControll /
    Added:
    * types className, component
    * props children, className
    * styles className
    
    Refactor:
    * Change component Button to component IconButton
    * Add arrows MdArrowLeft and MdArrowRight

--- client/src/components/Slider/components/SliderControll/SliderControll.tsx
+++ client/src/components/Slider/components/SliderControll/SliderControll.tsx
@@ -1,23 +1,35 @@
 import React from "react";
-import Button from "../../../../UI/Button/Button";
+import IconButton from "../../../../UI/IconButton/IconButton";
+import { MdArrowLeft, MdArrowRight } from "react-icons/md";
 import clsx from "clsx";
 import classes from '../../Slider.module.scss';
 
 
 interface ISliderControllProps {
+    component?: 'prev' | 'next';
     children?: React.ReactNode;
+    className?: string;
     onClick?: () => void;
 }
 
-export default function SliderControll(props) {
-    const { component, onClick } = props;
+export default function SliderControll(props: ISliderControllProps) {
+    const { 
+        component, 
+        children, 
+        className, 
+        onClick 
+    } = props;
 
     const styles = clsx(classes.controll, {
         [classes.buttonPrev]: component === 'prev',
         [classes.buttonNext]: component === 'next',
+        [className]: className
     })
 
     return (
-        <Button className={ styles } onClick={ onClick }>+</Button>
+        <IconButton className={ styles } onClick={ onClick }>
+            {component === 'prev' && <MdArrowLeft />}
+            {component === 'next' && <MdArrowRight />}
+        </IconButton>
     )
 }
