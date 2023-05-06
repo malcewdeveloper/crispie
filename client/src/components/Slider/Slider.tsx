0c97bf40e66f5f306faf464a02e47b40e3ef5245
Maksim <malcewdeveloper@yandex.ru>
2023-05-07 00:13:21 +0300

    [Slider] Refactor the component (client) /
    Refactor:
    * func handleClickNext, handleClickPrev
    * render slides
    
    Added:
    * autoplay slider
    * infinity slider
    * scrolling slides
    * state of the slider for scrolling slides
    * type StateSlider

--- client/src/components/Slider/Slider.tsx
+++ client/src/components/Slider/Slider.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import SliderItem from './components/SliderItem/SliderItem';
 import SliderControll from './components/SliderControll/SliderControll';
 import SliderTrack from './components/SliderTrack/SliderTrack';
+import { getSliderChildren } from './helpers/getSliderChildren';
 import clsx from 'clsx';
 import gsap from 'gsap';
 import classes from './Slider.module.scss';
@@ -11,59 +12,155 @@ interface ISliderProps {
     autoPlay?: boolean;
     autoPlayTime?: number;
     children?: React.ReactNode;
-    style?: React.CSSProperties;
     height?: number;
+    infinity?: boolean;
     width?: number;
+    style?: React.CSSProperties;
     spaceBetween?: number;
     slidesView?: number;
+    slidesScroll?: number;
+}
+
+type StateSlider = {
+    transitionValue: number;
+    restWidthScroll: number;
 }
 
 export default function Slider(props: ISliderProps): ReturnType<React.FC> {
     const { 
+        autoPlay,
+        autoPlayTime=3000,
         children, 
         style, 
-        height, 
+        height,
+        infinity,
         width, 
         spaceBetween, 
-        slidesView=1
+        slidesView=1,
+        slidesScroll=1
     } = props;
 
     const styles = clsx(classes.root, {
 
     })
-
-    const [currentSlide, setCurrentSlide] = React.useState(0);
+    
+    const [currentSlide, setCurrentSlide] = React.useState<number>(0);
     const sliderRef = React.useRef();
-    const sliderTrackRef = React.useRef();
+    const sliderTrackRef = React.useRef<HTMLUListElement>();
+
+    const slideWidth = 100 / slidesView; // ширина одного слайда
+    const totalSlides = React.Children.count(children); // кол-во слайдов
+    const totalTrackWidth = totalSlides * slideWidth; // общая ширина трека слайдреа
+    const touchWidth = slidesScroll * slideWidth; //длина прокрутки
+    const [{restWidthScroll, transitionValue}, setStateSlider] = React.useState<StateSlider>({
+        transitionValue: 0,
+        restWidthScroll: totalTrackWidth - (slidesScroll * slideWidth)
+    });
+
+    const slides = getSliderChildren(children);
 
     const handleClickNext = () => {
-        if(currentSlide === React.Children.count(children) - 1) {   // Временно в коде используется Children.count() 
-            setCurrentSlide(0)                                      // избавить от нее в дальшнейшем 
-            return
-        }
-        setCurrentSlide((prev) => prev + 1);
+        setCurrentSlide((currentSlide) => {
+            const nextSlide = currentSlide + slidesScroll; // индекс следующего слайда
+            const slidesRight = totalSlides - (currentSlide + slidesView); // кол-во слайдов справа
+
+            setStateSlider((stateSlider) => {
+                if(stateSlider.restWidthScroll < touchWidth && stateSlider.restWidthScroll > 0) {
+                    return {
+                        ...stateSlider,
+                        restWidthScroll: 0,
+                        transitionValue: stateSlider.transitionValue + stateSlider.restWidthScroll
+                    }
+                }
+                return {
+                    ...stateSlider, 
+                    restWidthScroll: stateSlider.restWidthScroll - touchWidth, 
+                    transitionValue: stateSlider.transitionValue + touchWidth
+                }
+            })  
+                                        
+            if(slidesRight <= 0) {
+                if(infinity) {
+                    setStateSlider({
+                        restWidthScroll: totalTrackWidth - (slidesScroll * slideWidth),
+                        transitionValue: 0
+                    })
+                    return 0;
+                } else {
+                    setStateSlider({
+                        restWidthScroll: 0,
+                        transitionValue: totalTrackWidth - (slidesScroll * slideWidth)
+                    })
+                    return currentSlide
+                }
+            }
+
+            return nextSlide
+        })
     }
 
     const handleClickPrev = () => {
-        if(currentSlide <= 0) {
-            setCurrentSlide(React.Children.count(children))
-        }
-        setCurrentSlide((prev) => prev - 1);
+        setCurrentSlide((currentSlide) => {
+            const nextSlide = currentSlide - slidesScroll;
+            const slidesLeft = currentSlide > 0 ? currentSlide : 0;
+
+            setStateSlider((stateSlider) => {
+                if(stateSlider.transitionValue < touchWidth && stateSlider.transitionValue > 0) {
+                    return {
+                        ...stateSlider,
+                        restWidthScroll: stateSlider.restWidthScroll + stateSlider.transitionValue,
+                        transitionValue: stateSlider.transitionValue - stateSlider.transitionValue
+                    }
+                }
+                return {
+                    ...stateSlider,
+                    restWidthScroll: stateSlider.restWidthScroll + touchWidth,
+                    transitionValue: stateSlider.transitionValue - touchWidth
+                }
+            })
+            
+            if(slidesLeft <= 0) {
+                if(infinity) {
+                    setStateSlider({
+                        restWidthScroll: 0,
+                        transitionValue: restWidthScroll
+                    });
+                    return totalSlides - slidesScroll;
+                } else {
+                    setStateSlider({
+                        restWidthScroll: restWidthScroll,
+                        transitionValue: 0
+                    })
+                    return currentSlide
+                }
+            }
+            return nextSlide
+        })
     }
 
     React.useEffect(() => {
         gsap.fromTo(
             sliderTrackRef.current,
             {
-                xPercent: -currentSlide * (100 / slidesView)
+                xPercent: -transitionValue
             },
             {
-                xPercent: -currentSlide * (100 / slidesView),
+                xPercent: -transitionValue,
                 duration: 0.2,
                 ease: 'power2.out'
             }
         )
     }, [currentSlide]);
+
+    React.useEffect(() => {
+        let timerId: NodeJS.Timer;
+        if(autoPlay) {
+            timerId = setInterval(() => {
+                handleClickNext();
+            }, autoPlayTime);
+        }
+        return () => clearInterval(timerId)
+    }, [autoPlay, autoPlayTime])
 
     return (
         <div className={ styles } style={{
@@ -72,20 +169,12 @@ export default function Slider(props: ISliderProps): ReturnType<React.FC> {
             ...style
         }} ref={ sliderRef }>
             <SliderTrack ref={ sliderTrackRef }>
-                { children }
+                {slides.map((slide, index) => {
+                    return React.cloneElement(slide, { style: { width: slideWidth + '%' } })
+                })}
             </SliderTrack>
             <SliderControll component='prev' onClick={ () => handleClickPrev() } />
             <SliderControll component='next' onClick={ () => handleClickNext() } />
         </div>
     )
 }
-
-/* 
-    В компоненте Slider необходимо доделать:
-    - autoPlay
-    - адаптивность под мобильные устройства
-    - сколько слайдов за раз показать
-    - на сколько слайдов за раз переключать
-    - бесконечный или нет
-    - контроллеры Dots, Progress
-*/
