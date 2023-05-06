4408ae5c33ab67e39b342bdf5e579d4a18e5e142
Maksim <malcewdeveloper@yandex.ru>
2023-05-07 00:05:40 +0300

    [SliderTrack] Add props className and change type ref (client)

--- client/src/components/Slider/components/SliderTrack/SliderTrack.tsx
+++ client/src/components/Slider/components/SliderTrack/SliderTrack.tsx
@@ -4,15 +4,20 @@ import classes from '../../Slider.module.scss';
 
 interface ISliderTrackProps {
     children?: React.ReactNode;
+    className?: string;
     style?: React.CSSProperties;
 }
 
 
-const SliderTrack = React.forwardRef((props: ISliderTrackProps, ref: any) => { // Изменить тип any
-    const { children, style } = props;
+const SliderTrack = React.forwardRef((props: ISliderTrackProps, ref: React.ForwardedRef<HTMLUListElement>) => {
+    const { 
+        children, 
+        className, 
+        style 
+    } = props;
 
     const styles = clsx(classes.track, {
-
+        [className]: className
     })
 
     return (
