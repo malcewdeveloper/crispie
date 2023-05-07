caea712f0a1766bb8d82c09b39b415577607b550
Maksim <malcewdeveloper@yandex.ru>
2023-05-07 19:09:04 +0300

    [root-router] Add router to work with slider requests (server)

--- server/routers/root-router.js
+++ server/routers/root-router.js
@@ -2,8 +2,10 @@ const Router = require('express');
 const rootRouter = new Router();
 const authRouter = require('./auth-router');
 const userRouter = require('./user-router');
+const sliderRouter = require('./slider-router');
 
 rootRouter.use('/', authRouter);
 rootRouter.use('/', userRouter);
+rootRouter.use('/', sliderRouter);
 
 module.exports = rootRouter;
