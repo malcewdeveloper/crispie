c125e985546f2448741110fee9e0d923ff109ff9
Maksim <malcewdeveloper@yandex.ru>
2023-05-15 23:34:35 +0300

    [App] Refactor rout for the path / (client)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import AppBar from "./modules/AppBar/AppBar";
+import Home from "./pages/Home/Home";
 
 
 export default function App() {
@@ -8,7 +9,7 @@ export default function App() {
         <Router>
             <AppBar />
             <Routes>
-                <Route path="/" element={<Link to='register'>Home</Link>} />
+                <Route path="/" element={<Home />} />
                 <Route path="/register" element={<Link to='login'>Rgister</Link>} />
                 <Route path="/login" element={<div>Login</div>} />
                 <Route path="*" element={<div>Error Page</div>} />
