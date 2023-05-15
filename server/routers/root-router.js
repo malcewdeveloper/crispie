b221c36766fcebe4823e885dfed994cc4cb55839
Maksim <malcewdeveloper@yandex.ru>
2023-05-15 14:21:45 +0300

    [root-router] Add productRouter to proccess product requests (server)

--- server/routers/root-router.js
+++ server/routers/root-router.js
@@ -3,9 +3,11 @@ const rootRouter = new Router();
 const authRouter = require('./auth-router');
 const userRouter = require('./user-router');
 const sliderRouter = require('./slider-router');
+const productsRouter = require('./products-router');
 
 rootRouter.use('/', authRouter);
 rootRouter.use('/', userRouter);
 rootRouter.use('/', sliderRouter);
+rootRouter.use('/', productsRouter);
 
 module.exports = rootRouter;
