6bafd4e0c6384e8a174c23ddb2359c162c917ef1
Maksim <malcewdeveloper@yandex.ru>
2023-05-15 14:52:49 +0300

    [database] Add three new database tables (server) /
    * products
    * categories
    * product_categories

--- server/database.sql
+++ server/database.sql
@@ -14,6 +14,45 @@ CREATE TABLE tokens (
     FOREIGN KEY (user_id) REFERENCES users (id)
 )
 
+CREATE TABLE categories (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    created_at TIMESTAMP DEFAULT now(),
+    updated_at TIMESTAMP DEFAULT now(),
+    url VARCHAR(255),
+    parent_category_id INTEGER,
+    FOREIGN KEY (parent_category_id) REFERENCES categories (id)
+)
+
+CREATE TABLE products (
+    id SERIAL PRIMARY KEY,
+    category_id INTEGER,
+    description TEXT,
+    discount_rate DECIMAL(10,2),
+    sku VARCHAR(255),
+    stock INTEGER,
+    title VARCHAR(255),
+    merchant VARCHAR(255),
+    price NUMERIC(10,2) DEFAULT 0,
+    promo_start_date TIMESTAMP DEFAULT NULL,
+    promo_end_date TIMESTAMP DEFAULT NULL,
+    is_sold BOOLEAN,
+    is_deleted BOOLEAN,
+    is_free_product BOOLEAN,
+    is_promoted BOOLEAN,
+    created_at TIMESTAMP DEFAULT now(),
+    updated_at TIMESTAMP DEFAULT now(),
+    rating INTEGER CHECK(rating>0 AND rating<=5),
+    FOREIGN KEY (category_id) REFERENCES categories (id),
+);
+
+CREATE TABLE product_categories (
+    category_id INTEGER,
+    product_id INTEGER,
+    FOREIGN KEY (category_name) REFERENCES categories (name),
+    FOREIGN KEY (product_id) REFERENCES products (id)
+)
+
 CREATE TABLE general_slider_content (
     id SERIAL PRIMARY KEY,
     image VARCHAR(255) DEFAULT NULL,
