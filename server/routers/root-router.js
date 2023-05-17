83ee97d3ead74b7ca8715449da30227bad85f9d7
Maksim <malcewdeveloper@yandex.ru>
2023-05-17 15:08:06 +0300

    [root-router] Add categoriesRouter (server)

--- server/routers/root-router.js
+++ server/routers/root-router.js
@@ -4,10 +4,12 @@ const authRouter = require('./auth-router');
 const userRouter = require('./user-router');
 const sliderRouter = require('./slider-router');
 const productsRouter = require('./products-router');
+const categoriesRouter = require('./categories-router');
 
 rootRouter.use('/', authRouter);
 rootRouter.use('/', userRouter);
 rootRouter.use('/', sliderRouter);
 rootRouter.use('/', productsRouter);
+rootRouter.use('/', categoriesRouter);
 
 module.exports = rootRouter;
