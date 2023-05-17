00e71f63dfd7353e37a537a862f6a99964f93265
Maksim <malcewdeveloper@yandex.ru>
2023-05-17 10:52:52 +0300

    [Menu] Add new props in the component and update style (client) /
    Properties:
    * className
    * style
    * droppable
    
    Style:
    * color: #fff
    * create a style for the droppable class

--- client/src/UI/Menu/Menu.tsx
+++ client/src/UI/Menu/Menu.tsx
@@ -3,22 +3,31 @@ import clsx from 'clsx';
 import classes from './Menu.module.scss';
 
 
-interface IMenuProps {
-    open?: boolean;
+export interface IMenuProps {
     children?: React.ReactNode;
-    anchorOrigin?: object;
+    className?: string;
+    droppable?: boolean;
+    style?: React.CSSProperties;
+    open?: boolean;
 }
 
 export default function Menu(props: IMenuProps): ReturnType<React.FC> {
 
-    const { children, open, anchorOrigin } = props;
+    const { 
+        children, 
+        className, 
+        droppable,
+        style,
+        open
+    } = props;
 
-    const styles = clsx({
-        [classes.root]: true,
-        [classes.open]: open
+    const styles = clsx(classes.root, {
+        [classes.open]: open,
+        [classes.droppable]: droppable,
+        [className]: className
     })
 
     return (
-        <div className={ styles }>{ children }</div>
+        <div className={ styles } style={ style }>{ children }</div>
     )
 }
