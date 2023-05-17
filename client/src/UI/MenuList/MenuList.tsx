100b8a997fdb4cffea3ae218b8292117cdb61dd9
Maksim <malcewdeveloper@yandex.ru>
2023-05-17 10:31:02 +0300

    [MenuList] Add new props in the component and update style (client) /
    Properties:
    * className
    * style
    * header
    
    Style:
    * color: inherit
    
    Component:
    * Add conditional rendering of the header

--- client/src/UI/MenuList/MenuList.tsx
+++ client/src/UI/MenuList/MenuList.tsx
@@ -1,21 +1,35 @@
 import React from "react";
+import Typography from "../Typography/Typography";
 import clsx from 'clsx';
 import classes from './MenuList.module.scss';
 
 
 interface IMenuListProps {
     children?: React.ReactNode;
+    className?: string;
+    header?: string;
+    style?: React.CSSProperties;
 }
 
 export default function MenuList(props: IMenuListProps): ReturnType<React.FC> {
     
-    const { children } = props;
+    const { 
+        children, 
+        className, 
+        header, 
+        style 
+    } = props;
 
     const styles = clsx({
         [classes.root]: true
     })
 
     return (
-        <ul className={ styles }>{ children }</ul>
+        <ul className={ styles } style={ style }>
+            {header && <Typography variant='h4' style={{ padding: '6px 16px 16px' }}>{ header }</Typography>}
+            { children }
+        </ul>
     )
 }
+
+Add new props in the component and update style  
