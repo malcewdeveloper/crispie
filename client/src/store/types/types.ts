5d693b4c6a591e8fbee196e32f659037bb55dae7
Maksim <malcewdeveloper@yandex.ru>
2023-06-06 15:52:24 +0300

    [chore] Create global file with types for Redux (client)

---
+++ client/src/store/types/types.ts
@@ -0,0 +1,6 @@
+export const AUTH_CHECK_SUCCESS = 'auth/checkSuccess';
+export const AUTH_REQUEST = 'auth/request';
+export const AUTH_REGISTER = 'auth/register';
+export const AUTH_LOGOUT = 'auth/logout';
+export const AUTH_ERROR = 'auth/error';
+export const AUTH_CHECK_ERROR = 'auth/checkError';
