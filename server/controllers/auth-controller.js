ed2bafe091cd6eaea531496a3b89e188ba490d0a
Maksim <malcewdeveloper@yandex.ru>
2023-06-06 18:10:51 +0300

    [auth-controller] Add save accessToken to cokkies (server)

--- server/controllers/auth-controller.js
+++ server/controllers/auth-controller.js
@@ -1,28 +1,37 @@
 const authService = require('../services/auth-service');
 const { validationResult } = require('express-validator');
 const ApiError = require('../exceptions/api-error');
+const UserDto = require('../dtos/UserDto');
 
 class AuthController {
     async registration(req, res, next) {
         try {
             const errors = validationResult(req);
             if(!errors.isEmpty()) {
-                return next(ApiError.BadRequest('Ошибка при валидации', errors));
+                return next(ApiError.BadRequest('Ошибка при валидации', errors.mapped()));
             }
             const { email, password } = req.body;
-            const newUser = await authService.registration(email, password);
-            res.cookie('refreshToken', newUser.refreshToken, {maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true});
-            return res.status(200).json(newUser);
+            const { refreshToken, accessToken, user } = await authService.registration(email, password);
+            const userDto = new UserDto(user.id, user.email);
+            res.cookie('accessToken', accessToken, {maxAge: 30 * 60 * 1000, httpOnly: true});
+            res.cookie('refreshToken', refreshToken, {maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true});
+            res.status(200).json({ redirectUrl: `${ process.env.CLIENT_URL }`, user: userDto });
         } catch(error) {
             next(error);
         }
     }
     async login(req, res, next) {
         try {
+            const errors = validationResult(req);
+            if(!errors.isEmpty()) {
+                return next(ApiError.BadRequest('Ошибка при валидации', errors.mapped()));
+            }
             const { email, password } = req.body;
-            const userData = await authService.login(email, password);
-            res.cookie('refreshToken', userData.refreshToken, {maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true});
-            return res.status(200).json(userData);
+            const { accessToken, refreshToken, user } = await authService.login(email, password);
+            const userDto = new UserDto(user.id, user.email)
+            res.cookie('accessToken', accessToken, { maxAge: 30 * 60 * 1000, httpOnly: true })
+            res.cookie('refreshToken', refreshToken, { maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true });
+            return res.status(200).json({ redirectUrl: `${ process.env.CLIENT_URL }`, user: userDto });
         } catch (error) {
             next(error)
         }
@@ -41,7 +50,8 @@ class AuthController {
             const { refreshToken } = req.cookies;
             const token = await authService.logout(refreshToken);
             res.clearCookie('refreshToken');
-            return res.status(200).json(token);
+            res.clearCookie('accessToken');
+            return res.status(200).json({ status: 200, statusText: 'OK' });
         } catch (error) {
             next(error);
         }
@@ -49,9 +59,11 @@ class AuthController {
     async refresh(req, res, next) {
         try {
             const { refreshToken } = req.cookies;
-            const userData = await authService.refresh(refreshToken);
-            res.cookie('refreshToken', userData.refreshToken, {maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true});
-            return res.status(200).json(userData);
+            const { user, refreshToken: newRefreshToken, accessToken: newAccessToken } = await authService.refresh(refreshToken);
+            const userDto = new UserDto(user.id, user.email);
+            res.cookie('accessToken', newAccessToken, { maxAge: 30 * 60 * 1000, httpOnly: true });
+            res.cookie('refreshToken', newRefreshToken, { maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true });
+            return res.status(200).json({ status: 200, statusText: 'OK', user: userDto });
         } catch (error) {
             next(error)
         }
