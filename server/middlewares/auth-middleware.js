381e9b1fdae95ac3707a09a3ea75963d7dc91d40
Maksim <malcewdeveloper@yandex.ru>
2023-06-06 17:57:19 +0300

    [auth-middleware] Reafactor: Replace the header authorization to cookies (server)

--- server/middlewares/auth-middleware.js
+++ server/middlewares/auth-middleware.js
@@ -2,17 +2,13 @@ const ApiError = require('../exceptions/api-error');
 const tokenService = require('../services/token-service');
 module.exports = function(req, res, next) {
     try {
-        const authorizationHeader = req.headers.authorization;
-        if(!authorizationHeader) {
-            return next(ApiError.UnauthorizedError());
-        }
-
-        const accessToken = authorizationHeader.split(' ')[1];
+        const { accessToken } = req.cookies;
         if(!accessToken) {
             return next(ApiError.UnauthorizedError());
         }
 
         const userData = tokenService.validateAccessToken(accessToken);
+        
         if(!userData) {
             return next(ApiError.UnauthorizedError());
         }
