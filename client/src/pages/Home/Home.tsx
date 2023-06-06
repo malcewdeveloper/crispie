db80d6b4ee090559a6397ad91b3db4fa75eeaa2d
Maksim <malcewdeveloper@yandex.ru>
2023-06-06 16:45:16 +0300

    [Home] Add the ToastContainer from  library react-toastify (client)

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Container from "../../UI/Container/Container";
 import HomeSlider from "../../modules/HomeSlider/HomeSlider";
 import CardList from "../../modules/CardList/CardList";
+import { ToastContainer } from 'react-toastify';
 
 
 const Home = (): ReturnType<React.FC> => {
@@ -13,6 +14,17 @@ const Home = (): ReturnType<React.FC> => {
             <CardList category="Авто" />
             <CardList category="Дом и сад" />
             <CardList category="Одежда, обвуь и аксессуары " />
+            <ToastContainer
+            position="top-right"
+            autoClose={5000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss={false}
+            draggable
+            pauseOnHover={false}
+            theme="light" />
         </Container>
     )
 }
