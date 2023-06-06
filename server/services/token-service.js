667bc60b553283b870d94294f7bc160baec26921
Maksim <malcewdeveloper@yandex.ru>
2023-06-06 17:14:02 +0300

    [token-service] Update time life to accessToken in func generateToken (server)

--- server/services/token-service.js
+++ server/services/token-service.js
@@ -4,7 +4,7 @@ const db  = require('../db');
 
 class TokenService {
     generateToken(payload) {
-        const accessToken = jwt.sign(payload, process.env.JWT_ACCESS_SECRET, {expiresIn: '30s'});
+        const accessToken = jwt.sign(payload, process.env.JWT_ACCESS_SECRET, {expiresIn: '30m'});
         const refreshToken = jwt.sign(payload, process.env.JWT_REFRESH_SECRET, {expiresIn: '30d'});
         return {
             accessToken,
