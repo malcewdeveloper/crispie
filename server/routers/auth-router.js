ead01df524d5bdf49def97ff38ea59bfce8326f9
Maksim <malcewdeveloper@yandex.ru>
2023-06-06 17:49:05 +0300

    [auth-router] Add express-validator to the login and register request (server)

--- server/routers/auth-router.js
+++ server/routers/auth-router.js
@@ -1,14 +1,33 @@
 const Router = require('express');
 const authRouter = new Router();
 const authController = require('../controllers/auth-controller');
-const { body } = require('express-validator');
+const { check } = require('express-validator');
 
 authRouter.post('/register', 
-    body('email').isEmail(), 
-    body('password').isLength({min: 3}), 
+    check('email', 'Неверный логин').isEmail(), 
+    check('password')
+        .isLength({ min: 6 })
+        .withMessage('Пароль должен содержать минимум 6 символов')
+        .matches(/[A-Z]/)
+        .withMessage('Пароль должен содержать хотя бы одну заглавную букву')
+        .matches(/[a-z]/)
+        .withMessage('Пароль должен содержать хотя бы одну строчную букву')
+        .matches(/\d/) 
+        .withMessage('Пароль должен содержать хотя бы одну цифру'),
     authController.registration
 );
-authRouter.post('/login', authController.login);
+authRouter.post('/login',
+    check('email', 'Неверный логин').isEmail(), 
+    check('password')
+        .isLength({ min: 6 })
+        .withMessage('Пароль должен содержать минимум 6 символов')
+        .matches(/[A-Z]/)
+        .withMessage('Пароль должен содержать хотя бы одну заглавную букву')
+        .matches(/[a-z]/)
+        .withMessage('Пароль должен содержать хотя бы одну строчную букву')
+        .matches(/\d/) 
+        .withMessage('Пароль должен содержать хотя бы одну цифру'),
+    authController.login);
 authRouter.post('/logout', authController.logout);
 authRouter.get('/activate/:link', authController.activate);
 authRouter.get('/refresh', authController.refresh);
