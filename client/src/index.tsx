ad1355c8450a661f7033285d16eac15ec2bb13c3
Maksim <malcewdeveloper@yandex.ru>
2023-06-06 16:16:04 +0300

    [chore] Add redux-store and add toastify styles (client)

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import store from "./store/store";
+import { Provider } from "react-redux";
 import './styles/global.scss';
+import './styles/toastify.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    <Provider store={ store }>
+        <App />
+    </Provider>
 )
