8d29b03499e4c90a7b043254664ac507b1c30b53
Maksim <malcewdeveloper@yandex.ru>
2023-06-06 16:13:28 +0300

    [App.tsx] Add Redux logic (client) /
    Added:
    * hook useDispatch
    * action checkAuth
    * checked authorization logic

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,31 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import AppBar from "./modules/AppBar/AppBar";
-import Footer from './components/Footer/Footer';
 import Home from "./pages/Home/Home";
+import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
+import Footer from './components/Footer/Footer';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { checkAuth } from "./store/actions/authActions";
+import { useDispatch } from "react-redux";
 
 
 export default function App() {
+    const dispatch = useDispatch();
+
+    React.useEffect(() => {
+        (async () => {
+            dispatch( await checkAuth())
+        })(); 
+    }, []);
+
     return (
         <Router>
             <AppBar />
             <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/register" element={<Link to='login'>Rgister</Link>} />
-                <Route path="/login" element={<div>Login</div>} />
-                <Route path="*" element={<div>Error Page</div>} />
+                <Route path="/" element={ <Home /> } />
+                <Route path="/register" element={ <Register /> } />
+                <Route path="/login" element={ <Login /> } />
+                <Route path="*" element={ <div>Error Page</div> } />
             </Routes>
             <Footer />
         </Router>
